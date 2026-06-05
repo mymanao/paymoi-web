58ce822e0340ad5740fd18f07934f0e2e0138c5e
tinarskii <tinvv@outlook.co.th>
2026-06-06 00:57:42 +0700

    feat: add Modal

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import {Web3AuthProvider} from '@web3auth/modal/vue'
 import {web3AuthContextConfig} from './web3authContext'
 import Navbar from "./fragments/Navbar.vue";
 import Footer from "./fragments/Footer.vue";
+import Modal from "./fragments/Modal.vue";
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import Footer from "./fragments/Footer.vue";
     <RouterView v-if="$route.path.startsWith('/overlay/')"/>
     <Web3AuthProvider v-else :config="web3AuthContextConfig">
       <Navbar v-if="!$route.path.startsWith('/donate') && !$route.path.startsWith('/overlay')"/>
+      <Modal />
       <RouterView/>
       <Footer v-if="!$route.path.startsWith('/donate') && !$route.path.startsWith('/overlay')"/>
     </Web3AuthProvider>
