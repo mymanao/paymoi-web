57c244c5ddf7c1f0a890d00ca0ea4d1776ca689b
tinarskii <tinvv@outlook.co.th>
2026-06-06 00:56:36 +0700

    feat: add Modal dialog

---
+++ src/fragments/Modal.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {
+  closeModal,
+  modalMessage,
+  modalOpen,
+  modalTitle
+} from "../composables/useModal.ts";
+</script>
+
+<template>
+  <dialog
+      class="modal"
+      :class="{ 'modal-open': modalOpen }"
+  >
+    <div class="modal-box">
+      <h3 class="font-bold text-lg text-center">
+        {{ modalTitle }}
+      </h3>
+
+      <p class="py-4">
+        {{ modalMessage }}
+      </p>
+
+      <div class="modal-action">
+        <button
+            class="btn btn-primary"
+            @click="closeModal"
+        >
+          ตกลง
+        </button>
+      </div>
+    </div>
+
+    <form
+        method="dialog"
+        class="modal-backdrop"
+    >
+      <button @click.prevent="closeModal">
+        close
+      </button>
+    </form>
+  </dialog>
+</template>
